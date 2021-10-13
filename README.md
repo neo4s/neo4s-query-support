6c89219bbdf60c2e2a57e261e3dac9fbca44371e
Brent Haines <thebrenthaines@yahoo.com>
2021-10-12 22:36:55 -0700

    Complete QueryTools specification and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
 # neo4s-query-support
+
+Implicits and types for helping with query parameter specifications, result handling and streaming, and other nice
+scala wrappers for java-based driver primitives and semantics.
+
+### Conversion of result set to a self-closing lazy list
+The following example makes use of the neo4s cypher dsl available at (TODO).
+``` scala 
+import aif.dna.neo4j.QueryTools._
+import aif.dna.cypher.dsl.syntax._
+
+import java.util.UUID
+
+case class Person(firstName: String, lastName: String, id: UUID)
+
+val anyPerson = any[Person]
+val query = cypher.MATCH(anyPerson).toQuery()
+
+// A durable session is not closed when we leave the `withDriver` scope
+val resultList = withDriver(_.withDurableSession(AccessMode.READ) { session =>
+     // Attaches the durabale session to the list
+     session.run(query.query,query.queryMap).recordList(session)
+})
+ 
+// The durable session is closed when the lazy list is at end. Forcing the lazy list will close it.
+resultList.foreach { record =>
+    println(record.get("person").asMap().asScala) 
+}
+```
+For a database that has two Person nodes present the example above yields:
+``` text
+Map(firstName -> Chuck, lastName -> Norris, id -> 614a23a5-00e2-4951-8689-b6fa1ca54f28)
+Map(firstName -> Vin, lastName -> Diesel, id -> 4a707a16-aba2-4ab7-aec4-e29b05efcf1d)
+```
+
+### Implicit lifting of result records to case classes
+``` scala 
+import aif.dna.neo4j.QueryTools._
+import aif.dna.cypher.dsl.syntax._
+
+import java.util.UUID
+
+case class Person(id: UUID, firstName: String, lastName: String)
+
+// Extraction behavior. Overrides for type conversion that is one post-read and pre-lift to node case classes
+implicit val extractionBehavior = ExtractionBehavior(typeMapper = {
+     case tuple@("id", idString: String) => (tuple._1,UUID.fromString(idString))
+})
+
+// Generates a typeclass extractor for the Person case class
+implicit val personExtractor = extractorFor[Person]
+
+val query = cyper.MATCH(any[Person]).toQuery()
+
+// The foregoing is all we need to define to get some really nice help from QueryTools...
+val people: LazyList[Person] = withDriver(_withDurableSession(AccessMode.READ) { session =>
+     // The person node will be labelled "person" in the generated query
+     session.run(query.query,query.queryMap).stream(session,"person")
+}
+
+people.foreach(println) // consume the lazy list to close the session
+```
+The foregoing yields the following output: 
+``` text
+Person(Chuck,Norris,a50d1008-889f-40e3-bfda-5719703331c1)
+Person(Vin,Diesel,5f1c7234-b81c-4089-b582-dc75d69ab780)
+```
+### We can also lift edge nodes and edges 
+Edges are `(node) -[:relation]-> (node)` patterns. The principle is exactly the same,
+but we need more extractors. There are two patterns that are supported; either including 
+the relationship for the edge or just the nodes. The following example
+provides for the latter.
+
+``` scala 
+case class Person(firstName: String, lastName: String, id: UUID)
+case class Article(name: String, descriptor: String)
+case class Wears(consistency: String)
+
+// Extraction behavior. Overrides for type conversion that is one post-read and pre-lift to node case classes
+implicit val extractionBehavior = ExtractionBehavior(typeMapper = {
+     case tuple@("id", idString: String) => (tuple._1,UUID.fromString(idString))
+})
+
+implicit val PersonExtractor = extractorFor[Person]
+implicit val DepartmentExtractor = extractorFor[Department]
+implicit val WorksForExtractor = extractorFor[WorksFor]
+
+val anyPerson = any[Person]
+val anyWears = any[Wears]
+val anyArticle = any[Article]
+
+val queryAllPeople = cypher
+     .MATCH(anyPerson -| anyWears |-> anyArticle)
+     .RETURN(anyPerson,anyWears,anyArticle)
+val query = queryAllPeople.toQuery()
+
+// Query returns a list of edges in the records. For instance a person who reports to two departments would be
+// LazyList((person: Person) -[wears:WEARS]-> (article: Article), (person: Person) -[wears:WEARS]-> (article: Article))
+
+val people = withDriver(_.withDurableSession { session =>
+     // Edges extracts the record items labelled "person", "works_for", and "department", which are leftnode, relationship, rightnode, respectively
+     // The type for the edge with these extractors is `(Person,Option[Wears,Article]))
+     session.run(query.query,query.queryMap).edges[Department,Wears,Article](session,"person","wears","article")
+})
+
+// Remember to exhaust the lazy list to close the session.. .
+people.foreach(println)
+```
+This example produces: 
+``` text
+(Person(Chuck,Norris,2f374677-4dae-47d9-9e50-2e69c75abe3c),Some((Wears(sometimes),Article(hat,black))))
+(Person(Chuck,Norris,2f374677-4dae-47d9-9e50-2e69c75abe3c),Some((Wears(usually),Article(boots,dusty))))
+(Person(Vin,Diesel,e8f5ba47-93d9-45a5-9597-3b848293defa),Some((Wears(always),Article(shirt,tight))))
+```
--- src/test/scala/neo4s/query/QueryToolsSpec.scala
+++ src/test/scala/neo4s/query/QueryToolsSpec.scala
@@ -1,6 +1,7 @@
 package neo4s.query
 
 import neo4s.query.ExtractionProvider.extractorFor
+import org.neo4j.driver.internal.value.NullValue
 import org.neo4j.driver.util.Resource
 import org.neo4j.driver.{Record, Result, Value}
 import org.scalatest.matchers.should
@@ -11,8 +12,6 @@ import java.util.UUID
 import scala.jdk.CollectionConverters._
 
 class QueryToolsSpec  extends AnyWordSpec with MockFactory with should.Matchers {
-
-  case class Person(firstName: String, lastName: String, id: UUID)
 
   def mockValueFor(map: Map[String,AnyRef]): Value = {
     val valueMock = mock[Value]
@@ -25,48 +24,91 @@ class QueryToolsSpec  extends AnyWordSpec with MockFactory with should.Matchers 
     val resultsIterator = resultsValueMap.iterator
 
     val resultMock = mock[Result]
-    (resultMock.hasNext _).expects().onCall( _ => resultsIterator.hasNext)
-    (resultMock.next _).expects().onCall { _ =>
+    (resultMock.hasNext _).expects().onCall( _ => resultsIterator.hasNext).repeat(resultsMap.size + 1)
+    (resultMock.next _)
+    .expects()
+    .onCall { _ =>
       val nextRecord = resultsIterator.next()
       val recordMock = mock[Record]
-      (recordMock.get(_:String)).expects(*).onCall { id: String => nextRecord(id) }
+      (recordMock.get(_:String))
+        .expects(*)
+        .onCall { id: String =>nextRecord.getOrElse(id,NullValue.NULL) }
+        .repeat(nextRecord.size)
       recordMock
     }
+    .repeat(resultsMap.size)
 
     resultMock
   }
 
-  def mockedPersonQuery(): Result = {
-    val chuckValues = Map("person" -> Map("firstName" -> "Chuck", "lastName" -> "Norris", "id" -> UUID.randomUUID()))
-    val vinValues = Map("person" -> Map("firstName" -> "Vin", "lastName" -> "Deisel", "id" -> UUID.randomUUID()))
+  case class Person(firstName: String, lastName: String, id: UUID)
 
-    val resultMap = Map("person" -> chuckValues) :: Map("person" -> vinValues) :: Nil
+  val chuckId = UUID.randomUUID()
+  val chuck = Person("Chuck", "Norris", chuckId)
+  val chuckValues = Map("person" -> Map("firstName" -> "Chuck", "lastName" -> "Norris", "id" -> chuckId))
+
+  val vinId = UUID.randomUUID()
+  val vin = Person("Vin", "Diesel", vinId)
+  val vinValues = Map("person" -> Map("firstName" -> "Vin", "lastName" -> "Diesel", "id" -> vinId))
+
+  val expectedPersons = Set(
+    chuck,
+    vin
+  )
+
+  case class Article(name: String, descriptor: String)
+  case class Wears(consistency: String)
+
+  val hat = Article("hat", "black")
+  val hatValues = Map("article" -> Map("name" -> "hat", "descriptor" -> "black"))
+
+  val shirt = Article("shirt", "tight")
+  val shirtValues = Map("article" -> Map("name" -> "shirt", "descriptor" -> "tight"))
+
+  val boots = Article("boots", "dusty")
+  val bootsValues = Map("article" -> Map("name" -> "boots", "descriptor" -> "dusty"))
+
+  val expectedEdgeNodes = Set(
+    (chuck, Some(hat)),
+    (chuck, Some(boots)),
+    (vin, Some(shirt))
+  )
+
+  val always = Wears("always")
+  val alwaysValues = Map("wears" -> Map("consistency" -> "always"))
+
+  val usually = Wears("usually")
+  val usuallyValues = Map("wears" -> Map("consistency" -> "usually"))
+
+  val sometimes = Wears("sometimes")
+  val sometimesValues = Map("wears" -> Map("consistency" -> "sometimes"))
+
+  val expectedEdges = Set(
+    (chuck, Some((sometimes,hat))),
+    (chuck, Some((usually,boots))),
+    (vin, Some((always,shirt)))
+  )
+
+  def mockedNodesQuery(): Result = {
+    val resultMap = chuckValues ::vinValues :: Nil
     mockResultsFor(resultMap)
+  }
 
-    val chuckMock = mockValueFor(chuckValues)
-    val vinMock =   mockValueFor(vinValues)
+  def mockedEdgeNodesQuery(): Result = {
+    val resultMap = (chuckValues ++ hatValues) :: (chuckValues ++ bootsValues) :: (vinValues ++ shirtValues) :: Nil
+    mockResultsFor(resultMap)
+  }
 
-    val resultMock = mock[Result]
-    val recordMock = mock[Record]
-
-    (recordMock.get(_:String)).expects("person").returning(chuckMock)
-    (recordMock.get(_:String)).expects("person").returning(vinMock)
-
-    // Set up mock for Result api...
-    (resultMock.hasNext _).expects().returning(true)
-    (resultMock.next _).expects().returning(recordMock)
-    (resultMock.hasNext _).expects().returning(true)
-    (resultMock.next _).expects().returning(recordMock)
-    (resultMock.hasNext _).expects().returning(false)
-
-    resultMock
+  def mockedEdgesQuery(): Result = {
+    val resultMap = (chuckValues ++ sometimesValues ++ hatValues) :: (chuckValues ++ usuallyValues ++ bootsValues) :: (vinValues ++ alwaysValues ++ shirtValues) :: Nil
+    mockResultsFor(resultMap)
   }
 
   "QueryTools" should {
     "derive a lazy list of records from a query result" in {
       import QueryTools._
 
-      val results = mockedPersonQuery()
+      val results = mockedNodesQuery()
       val resource = stub[Resource]
       val resultStream = results.recordList(resource)
 
@@ -77,7 +119,7 @@ class QueryToolsSpec  extends AnyWordSpec with MockFactory with should.Matchers 
       }
     }
 
-    "extract a lazy list of extracted values from a query result" in {
+    "extract a lazy list of nodes from a query result" in {
       import QueryTools._
 
       implicit val extractionBehavior: ExtractionBehavior = ExtractionBehavior(typeMapper = {
@@ -86,10 +128,50 @@ class QueryToolsSpec  extends AnyWordSpec with MockFactory with should.Matchers 
 
       implicit val personExtractor = extractorFor[Person]
 
-      val results = mockedPersonQuery()
+      val results = mockedNodesQuery()
       val resource = stub[Resource]
 
-      results.valueList(resource,"person").foreach(v => println(v))
+      results.valueList(resource,"person").foreach { person =>
+        expectedPersons should contain (person)
+      }
+    }
+
+    "extract a lazy list of edge nodes from a query result" in {
+      import QueryTools._
+
+      implicit val extractionBehavior: ExtractionBehavior = ExtractionBehavior(typeMapper = {
+        case tuple@("id", idString: String) => (tuple._1,UUID.fromString(idString))
+      })
+
+      implicit val personExtractor = extractorFor[Person]
+      implicit val articleExtractor = extractorFor[Article]
+
+      val results = mockedEdgeNodesQuery()
+      val resource = stub[Resource]
+
+      results.edges[Person,Article](resource, "person", "article").foreach { edgeNodes =>
+        expectedEdgeNodes should contain (edgeNodes)
+      }
+    }
+
+    "extract a lazy list of edges from a query result" in {
+      import QueryTools._
+
+      implicit val extractionBehavior: ExtractionBehavior = ExtractionBehavior(typeMapper = {
+        case tuple@("id", idString: String) => (tuple._1,UUID.fromString(idString))
+      })
+
+      implicit val personExtractor = extractorFor[Person]
+      implicit val wearsExtractor = extractorFor[Wears]
+      implicit val articleExtractor = extractorFor[Article]
+
+      val results = mockedEdgesQuery()
+      val resource = stub[Resource]
+
+      results.edges[Person,Wears,Article](resource, "person", "wears", "article").foreach { edge =>
+        println(edge)
+        expectedEdges should contain (edge)
+      }
     }
   }
 
